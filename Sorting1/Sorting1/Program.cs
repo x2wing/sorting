126b8db280fec93e715d257eea4f67720555225e
x2wing <x2wing@gmail.com>
2017-02-03 16:55:16 +0500

    323434

--- Sorting1/Sorting1/Program.cs
+++ Sorting1/Sorting1/Program.cs
@@ -10,7 +10,7 @@ namespace Sorting1
     {
         static void Main(string[] args)
         {
-            int[] V = { 4, 6, 1, 3, 5, 7, 9, 5, 4, 23, 4, 6, 47, 568 };
+            int[] V = { 3234234,4, 6, 1, 3, 5, 7, 9, 5, 4, 23, 4, 6, 47, 568 };
 
 
             foreach (int i in V)
